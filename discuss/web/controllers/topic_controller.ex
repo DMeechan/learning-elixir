669460981592e48cad1775f67b9298639c4de200
Daniel Meechan <daniel.meechan.1@gmail.com>
2018-12-21 04:49:00 +0000

    Created new.html route and file

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,7 +1,13 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
-  def new(conn, _params) do
-    return conn
+  # The alias lets us write 'Topic' instead of 'Discuss.Topic'
+  alias Discuss.Topic
+
+  def new(conn, params) do
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
+    render conn, "new.html"
   end
 end
