a5cc823c8d3d6c8f357ba23c5cf863e2f7d1fd89
Daniel Meechan <daniel.meechan.1@gmail.com>
2018-12-27 07:40:33 +0000

    Added Users for auth

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,10 +2,25 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts("CALLBACK")
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
-    IO.puts("END")
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "github"
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
