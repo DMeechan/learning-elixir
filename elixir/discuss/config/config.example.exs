7aca128b197c4408c76ec60eb9ab6f92e468c20e
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-01-23 01:07:34 +0000

    Added example exs file

---
+++ elixir/discuss/config/config.example.exs
@@ -0,0 +1,41 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "tIZgEpKcJbp/y2Br3GTTxn7/frzVWzW+lUGIZnCj8c3GuRNER9aSkDBtFbRldTdS",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "config.secret.exs"
+import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: Application.get_env(:discuss, :github_client_id),
+  client_secret: Application.get_env(:discuss, :github_client_secret)
+
+# Source on how to set up own *.secret.exs file
+# And then importing that:
+# https://stackoverflow.com/a/47223081
