bb5c8af42dfae257a760b8aa7f973ecdb06de53a
Daniel Meechan <dm280@st-andrews.ac.uk>
2019-02-18 14:26:20 +0000

    Created parser test

--- elixir/lispex/test/parser_test.exs
+++ elixir/lispex/test/parser_test.exs
@@ -2,4 +2,24 @@ defmodule Lispex.ParserTest do
   use ExUnit.Case
   doctest Lispex
 
+  alias Lispex.Parser
+
+  test "parses tokens into tree" do
+    # test with LISP code: (begin (define r 10) (* r r))
+    input = ["(", "begin", "(", "define", "r", "10", ")", "(", "*", "r", "r", ")", ")"]
+    acc = []
+    output = Parser.parse(input, acc)
+
+    expected_output = [[:begin, [:define, :r, 10], [:*, :r, :r]]]
+    assert expected_output == output
+
+    # test with LISP code: (begin (if (> x y) (set! max x) (set! max y)))
+    input = [ "(", "begin", "(", "if", "(", ">", "x", "y", ")", "(", "set!", "max", "x", ")", "(", "set!", "max", "y", ")", ")", ")"]
+    acc = []
+    output = Parser.parse(input, acc)
+
+    expected_output = [[:begin, [:if, [:>, :x, :y], [:set!, :max, :x], [:set!, :max, :y]]]]
+    assert expected_output == output
+  end
+
 end
