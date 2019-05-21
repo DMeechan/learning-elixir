8cd69d1227c436529c5585703a97bf2e4c887f10
Daniel Meechan <dm280@st-andrews.ac.uk>
2019-05-21 17:50:32 +0100

    Added Board initialization

---
+++ chess-react/src/components/pieces/index.js
@@ -0,0 +1,10 @@
+import Bishop from './bishop.js';
+import King from './king.js';
+import Knight from './knight.js';
+import Pawn from './pawn.js';
+import Queen from './queen.js';
+import Rook from './rook.js';
+
+export default {
+    Bishop, King, Knight, Pawn, Queen, Rook
+};
